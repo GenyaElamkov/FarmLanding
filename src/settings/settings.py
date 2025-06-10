80312515dc07ca06d0e66a290830ad0d4a203b29
GenyaElamkov <genyaElamkov@gmail.com>
2025-06-10 11:49:39 +0300

    refactoring code

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -18,6 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 
     'lending.apps.LendingConfig',
 
